2c4732285266143cbed5f3461dc5745167f2bd27
athuljain <athuljainkj2@gmail.com>
2025-04-15 15:21:40 +0530

    view menu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
@@ -7,11 +7,18 @@ import AppDownload from '../../components/AppDownload/AppDownload'
 const Home = () => {
 
   const [category,setCategory] = useState("All")
-
+  const menuRef = useRef(null);
   return (
     <>
-      <Header/>
-      <ExploreMenu setCategory={setCategory} category={category}/>
+      {/* <Header/> */}
+      {/* <ExploreMenu setCategory={setCategory} category={category}/> */}
+      <Header scrollToMenu={() => menuRef.current.scrollIntoView({ behavior: "smooth" })} />
+     
+
+      <div ref={menuRef}> {/* Step 2: Attach ref here */}
+        <ExploreMenu setCategory={setCategory} category={category} />
+      </div>
+
       <FoodDisplay category={category}/>
       <AppDownload/>
     </>
